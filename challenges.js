94a3d2ed77a5b3caab367f37c7e1e426eec80d75
iwona <iwonalafave@gmail.com>
2023-03-31 19:10:36 -0700

    challengesDone

--- challenges.js
+++ challenges.js
@@ -1,21 +1,54 @@
 // Create a variable called myName and assign it your name as a string.
+const myName = "Tomek";
 
 // Create an array called numbers and add five numbers to it.
 
 // Create a variable called myAge and assign it your age as a number.
+const myAge = 9;
 
 // Create an object called person with properties name, age, and gender.
+const person = {
+    name: "Tomek",
+    age: 9,
+    gender: "male",
+}
+console.log(person)
 
 // Create a variable called isRaining and assign it a boolean value based on whether it is currently raining or not.
+let isRaining = false;
 
 // Create a function called greet that takes a name as a parameter and returns a greeting message using template literals.
+function greet(name){
+return `Witam moj drogi ${name}`
+}
+console.log(greet("Tomku"))
 
 // Create a function called multiply that takes two numbers as parameters and returns their product.
+function multiply(a,b){
+    return a*b
+}
+console.log(multiply(12,53))
 
 // Create a function called divide that takes two numbers as parameters and returns their quotient.
+function divide(c,d){
+    return c/d
+}
+console.log(divide(64,8))
 
 // BONUS
 // Create a function called isEven that takes a number as a parameter and returns true if the number is even, false otherwise.
+ function isEven(number){
+    if (number%2 ===0){
+        return true
+    }else{
+         return false
+    }
+}
+console.log(isEven(9))
 
+
+    
+
+ 
 
 
